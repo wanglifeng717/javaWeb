5d09907884ce76c21499c59301659025809c56b1
wanglifeng717 <2498202385@qq.com>
2018-01-10 15:45:41 +0800

    maven构建javaWeb开发环境
    1.ecilipse建maven工程,选war包格式。
    2.按照图示修改工程，使其符合web工程规范
    3.pom.xml添加东西，因为没有servlet运行时环境
     注意：servlet和JSP一定是provided，这样打包就不会放到target的lib下面,不然运行时一定报错的。因为和tomcat自带的发生的jar包冲突。

---
+++ src/main/java/com/tongji/test/Person.java
@@ -0,0 +1,23 @@
+/**
+* Title: Person.java
+* Description: 
+* Copyright: Copyright (c) 2017
+* Company: TongjiUniversity
+* @author mdm(computer in lab)
+* @date Jan 8, 2018
+* @version 1.0
+*/
+package com.tongji.test;
+
+/**  
+* Title: Person 
+* Description:  
+* @author mdm(computer in lab)  
+* @date Jan 8, 2018  
+*/
+public class Person {
+
+	public String getPerionInfo() {
+		return "i am a person .... hello web world";
+	}
+}
