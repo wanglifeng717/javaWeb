31fcdc3461bcdba1fdce21f2bf1b5968a0c1e56b
wanglifeng717 <2498202385@qq.com>
2018-01-11 21:43:12 +0800

    练习：用户名密码验证练习
    题目：在 web.xml 文件中设置两个 WEB 应用的初始化参数, user, password.
    定义一个 login.html, 里边定义两个请求字段: user, password. 发送请求到 loginServlet
    在创建一个 LoginServlet, 在其中获取请求的 user, password. 比对其和 web.xml 文件中定义的请求参数是否一致
    若一致, 响应 Hello:xxx, 若不一致, 响应 Sorry: xxx  xxx 为 user.

--- src/main/java/com/tongji/test/loginServlet.java
+++ src/main/java/com/tongji/test/loginServlet.java
@@ -10,16 +10,14 @@
 package com.tongji.test;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.Map;
+import java.io.PrintWriter;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 
 /**  
 * Title: loginServlet 
@@ -27,15 +25,17 @@ import javax.servlet.http.HttpServletRequest;
 * @author mdm(computer in lab)  
 * @date Jan 11, 2018  
 */
-public class loginServlet implements Servlet{
+public class loginServlet implements Servlet {
 
 	/* (non-Javadoc)
 	 * @see javax.servlet.Servlet#init(javax.servlet.ServletConfig)
 	 */
+	//为了在其他方法里面获得全局的配置参数，我们必须把这个对象拿出来。
+	private  ServletConfig servlerconfig;
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		// TODO Auto-generated method stub
-		
+		this.servlerconfig=config;
 	}
 
 	/* (non-Javadoc)
@@ -52,78 +52,20 @@ public class loginServlet implements Servlet{
 	 */
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-		System.out.println("有请求来了。。。。");
-		/*①. 获取请求参数: 
-
-			> String getParameter(String name): 根据请求参数的名字, 返回参数值. 
-			若请求参数有多个值(例如 checkbox), 该方法只能获取到第一个提交的值，所以用下面这个方法. 
-
-			> String[] getParameterValues(String name): 根据请求参数的名字, 返回请求参数对应的字符串数组. 
-			
-			> Enumeration getParameterNames(): 返回参数名对应的 Enumeration 对象, 
-			类似于 ServletConfig(或 ServletContext) 的 getInitParameterNames() 方法. 
-			这个方法同样的问题就是，获取例如checkbox多值类型的还是只返回第一个。
-			
-			> Map getParameterMap(): 返回请求参数的键值对: key: 参数名,  value: 参数值, String 数组类型. 
-*/
-		//输入tongji  123
-		String name= req.getParameter("user");
-		System.out.println(name);//tongji
+		ServletContext servletContext = servlerconfig.getServletContext();
+		String initUser = servletContext.getInitParameter("user");
+		String initPassword = servletContext.getInitParameter("password");
 		
-		String[] interesting = req.getParameterValues("interesting");
-		System.out.println(Arrays.toString(interesting));//[game, party]
+		String user = req.getParameter("user");
+		String password = req.getParameter("password");
 		
-		Enumeration<String> names = req.getParameterNames();
-		while(names.hasMoreElements())
-		{
-			String name1 = names.nextElement();
-			String value = req.getParameter(name1);
-			System.out.println("Enumeration: "+name1+":"+value);
+		PrintWriter writer = res.getWriter();
+		if(user.equals(initUser) && password.equals(initPassword)) {
+			writer.print("hello "+user+" welcome");
+		} else {
+			writer.println("sorry,"+user+" your wrong!");
 		}
-		/*
-			Enumeration: user:tongji
-			Enumeration: password:123
-			Enumeration: interesting:game
-		*/
-		
-		Map<String, String[]> map = req.getParameterMap();
-		for(Map.Entry<String, String[]> entry:map.entrySet()) {
-			System.out.println("map: "+ entry.getKey()+Arrays.toString(entry.getValue()));
-		}
-				/*map: user[tongji]
-				map: password[123]
-				map: interesting[game, party]*/
-		
-		
-		/* 获取请求的 URI:*/		
-		HttpServletRequest httpServletRequest = (HttpServletRequest) req;
-		String requestURI=httpServletRequest.getRequestURI();
-		System.out.println(requestURI);///javaWeb/login
-		
-		/*获取请求方式:  
 
-			String method = httpServletRequest.getMethod();
-			System.out.println(method); //GET
-			
-		④. 若是一个 GET 请求, 获取请求参数对应的那个字符串, 即 ? 后的那个字符串. 
-
-			String queryString = httpServletRequest.getQueryString();//如果是post 返回就是Null
-			System.out.println(queryString);
-			 //user=atguigu&password=1234&interesting=game&interesting=party&interesting=shopping
-*/
-		String method = httpServletRequest.getMethod();
-		System.out.println(method);//GET
-		
-		String queryString = httpServletRequest.getQueryString();
-		System.out.println(queryString);//user=tongji&password=123&interesting=game&interesting=party
-		//获取请求的 Serlvet 的映射路径 
-		String servletPath = httpServletRequest.getServletPath();
-		System.out.println(servletPath);// /login
-		
-		String remoteAddr = httpServletRequest.getRemoteAddr();
-		System.out.println(remoteAddr); //127.0.0.1
-		StringBuffer requestURL = httpServletRequest.getRequestURL();
-		System.out.println(requestURL);//http://127.0.0.1:8080/javaWeb/login
 	}
 
 	/* (non-Javadoc)
@@ -141,7 +83,7 @@ public class loginServlet implements Servlet{
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
